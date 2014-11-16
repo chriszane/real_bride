1f84ea7db2977c8accec7f94b5da024c543c788d
Zane <chriszane_villacorta@yahoo.com>
2014-11-16 16:46:29 +0800

    realbride theme

---
+++ wp-content/themes/realbride_theme/index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<head>
+	<title>Real Bride</title>
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+<body>
+	<div id="container"></div>
+</body>
+</html>
